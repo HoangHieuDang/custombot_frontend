1a7e366ea53f5da94c6c8f63f03416cdd6d5ccf0
Hieu Dang <hieudang@hieus-mbp.speedport.ip>
2025-05-17 21:17:01 +0200

    adding 3d parts and extend custombot to test adding 3d parts into the scene for CustomBot.jsx Component

--- src/CustomBot.jsx
+++ src/CustomBot.jsx
@@ -9,7 +9,7 @@ const CustomBot = () => {
       <h1>Create your Custom Bot</h1>
       <div className="h-3/4 w-1/2">
         <Canvas
-          style={{ backgroundColor: "#EFDCAB" }}
+          style={{ backgroundColor: "#BE5B50" }}
           camera={{ position: [0, 0, 15], rotation: [0, 0, 0], fov: 100 }}
         >
           <ambientLight intensity={5} />
@@ -32,15 +32,31 @@ const CustomBot = () => {
 
           {/* Chest */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_chest_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
           />
 
-          {/* Waist */}
+          {/* Upper Waist */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_upper_waist_1.gltf"}
+            scale={1}
+            position={[0, 0, 0]}
+            rotation={[0, Math.PI / 2, 0]}
+          />
+
+          {/* Lower Waist */}
+          <DynamicPart
+            url={"./src/assets/3d_assets/cb_lower_waist_1.gltf"}
+            scale={1}
+            position={[0, 0, 0]}
+            rotation={[0, Math.PI / 2, 0]}
+          />
+
+          {/* Shoulder */}
+          <DynamicPart
+            url={"./src/assets/3d_assets/cb_shoulder_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -48,7 +64,7 @@ const CustomBot = () => {
 
           {/* Upper_arm */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_upper_arm_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -56,7 +72,7 @@ const CustomBot = () => {
 
           {/* Lower_arm */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_lower_arm_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -64,7 +80,7 @@ const CustomBot = () => {
 
           {/* Hand */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_hand_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -72,7 +88,7 @@ const CustomBot = () => {
 
           {/* Side_skirt */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_side_skirt_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -80,7 +96,15 @@ const CustomBot = () => {
 
           {/* Front_skirt */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_front_skirt_1.gltf"}
+            scale={1}
+            position={[0, 0, 0]}
+            rotation={[0, Math.PI / 2, 0]}
+          />
+
+          {/* Back_skirt */}
+          <DynamicPart
+            url={"./src/assets/3d_assets/cb_back_skirt_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -88,7 +112,7 @@ const CustomBot = () => {
 
           {/* Upper_leg */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_upper_leg_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -96,7 +120,7 @@ const CustomBot = () => {
 
           {/* Lower_leg */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_lower_leg_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
@@ -104,7 +128,7 @@ const CustomBot = () => {
 
           {/* Foot */}
           <DynamicPart
-            url={"./src/assets/3d_assets/cb_head_1.gltf"}
+            url={"./src/assets/3d_assets/cb_foot_1.gltf"}
             scale={1}
             position={[0, 0, 0]}
             rotation={[0, Math.PI / 2, 0]}
